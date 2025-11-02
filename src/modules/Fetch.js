a98a8d15128787bf0f55874bf6dfd8919217fadf
Dan Arhipov <chifmatsuno17@gmail.com>
2025-11-02 20:29:51 +0300

    Add new tags & add error message & add fetch with tags

--- src/modules/Fetch.js
+++ src/modules/Fetch.js
@@ -10,7 +10,8 @@ class Fetch {
     imagesContainerSelector: `[data-js-images-container]`,
     imageSelector: `[data-js-image]`,
 
-    emptyMessage: '[data-js-empty-message]',
+    emptyMessageSelector: '[data-js-empty-message]',
+    errorMessageSelector: `[data-js-error-message]`,
 
   }
 
@@ -20,26 +21,30 @@ class Fetch {
     this.searchButtonElement = document.querySelector(this.selectors.searchButtonSelector)
     this.responseElement = document.querySelector(this.selectors.responseSelector)
     this.imagesContainerElement = document.querySelector(this.selectors.imagesContainerSelector)
-    this.imageElement = document.querySelector(this.selectors.imageSelector)
 
-    this.emptyMessage = document.querySelector(this.selectors.emptyMessage)
+    this.emptyMessageElement = document.querySelector(this.selectors.emptyMessageSelector)
+    this.errorMessageElement = document.querySelector(this.selectors.errorMessageSelector)
 
+    this.tags = []
 
     this.inputWrapperElement.addEventListener('tagsChanged', (event) => {
-      const tags = event.detail.tags
-      console.log('Selected tags:', tags)
+       this.tags = event.detail.tags
+      console.log('Selected tags:', this.tags)
     })
 
+    this.hideErrorMessage()
     this.updateEmptyMessageVisibility()
     this.bindEvents()
   }
 
   updateEmptyMessageVisibility() {
-    const haveImage = (this.imagesContainerElement.children.length - 1) > 0
-    if (haveImage) {
-      this.emptyMessage.setAttribute('empty', '')
+    const haveImage = (this.imagesContainerElement.children.length - 2) > 0
+    const hasError = !this.errorMessageElement.hasAttribute('empty')
+
+    if (haveImage || hasError) {
+      this.emptyMessageElement.setAttribute('empty', '')
     } else {
-      this.emptyMessage.removeAttribute('empty')
+      this.emptyMessageElement.removeAttribute('empty')
     }
   }
 
@@ -49,6 +54,12 @@ class Fetch {
     liElement.setAttribute('data-js-image', '')
     liElement.appendChild(img)
     this.imagesContainerElement.appendChild(liElement)
+
+    // Автоматически прокручиваем к новому элементу
+    liElement.scrollIntoView({
+      behavior: 'smooth',
+      block: 'nearest',
+    })
 
     this.updateEmptyMessageVisibility()
   }
@@ -66,18 +77,73 @@ class Fetch {
   }
 
   fetchLucky() {
+    // Скрываем сообщение об ошибке перед новым запросом
+    this.hideErrorMessage()
+
     fetch('https://cataas.com/cat?type=medium')
-      .then(response => response.blob())
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`${response.status}`)
+        }
+        return response.blob()
+      })
       .then(blob => this.createImgElement(blob))
-      .catch(error => console.log("Ошибка:" + error))
+      .catch(error => {
+        console.log(error)
+        this.showErrorMessage(error)
+      })
   }
 
-  onLuckyButtonClick() {
+  onLuckyButtonClick(event) {
+    event.preventDefault()
     this.fetchLucky()
   }
 
+  showErrorMessage(error) {
+    this.errorMessageElement.removeAttribute('empty')
+    this.errorMessageElement.textContent = 'Изображение с такими тегами не найдено! Код ошибки: ' + `${error}`
+    this.updateEmptyMessageVisibility()
+  }
+
+  hideErrorMessage() {
+    this.errorMessageElement.setAttribute('empty', '')
+    this.updateEmptyMessageVisibility()
+  }
+
+  fetchSearch() {
+    // Если тегов нет - получаем случайного кота
+    if (this.tags.length === 0) {
+      this.fetchLucky()
+      return
+    }
+
+    this.hideErrorMessage()
+
+    const tagsString = this.tags.join(',')
+    const url = `https://cataas.com/cat/${tagsString}?type=medium`
+
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`${response.status}`)
+        }
+        return response.blob()
+      })
+      .then(blob => this.createImgElement(blob))
+      .catch(error => {
+        console.log(error)
+        this.showErrorMessage(error)
+      })
+  }
+
+  onSearchButtonClick(event) {
+    event.preventDefault()
+    this.fetchSearch()
+  }
+
   bindEvents() {
-    this.luckyButtonElement.addEventListener('click', () => this.onLuckyButtonClick())
+    this.luckyButtonElement.addEventListener('click', (event) => this.onLuckyButtonClick(event))
+    this.searchButtonElement.addEventListener('click', (event) => this.onSearchButtonClick(event))
   }
 }
 
