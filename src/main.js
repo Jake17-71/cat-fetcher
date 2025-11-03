9fa57bb31a7f4237c273fdbd23c26020b48b4b68
Dan Arhipov <chifmatsuno17@gmail.com>
2025-11-03 16:27:02 +0300

    Add scroll-to-top button logic

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import './styles/styles.scss'
 import ThemeChangeCollection from './modules/ThemeChange.js'
 import TagsCollection from './modules/Tags.js'
 import FetchCollection from './modules/Fetch.js'
+import ScrollButtonCollection from './modules/ScrollButton.js'
 
 new ThemeChangeCollection()
 new TagsCollection()
 new FetchCollection()
+new ScrollButtonCollection()
---
+++ src/modules/ScrollButton.js
@@ -0,0 +1,60 @@
+const inputWrapperSelector = '[data-js-scroll-to-top]'
+
+class ScrollButton {
+
+  stateClasses = {
+    isVisible: 'is-visible',
+  }
+
+  constructor(rootElement) {
+    this.scrollToTopButtonElement = rootElement
+    this.scrollThreshold = 300
+
+    this.bindEvents()
+    this.checkScrollPosition()
+  }
+
+  checkScrollPosition() {
+    const scrollTop = window.scrollY || document.documentElement.scrollTop
+
+    if (scrollTop > this.scrollThreshold) {
+      this.show()
+    } else {
+      this.hide()
+    }
+  }
+
+  show() {
+    this.scrollToTopButtonElement.classList.add(this.stateClasses.isVisible)
+  }
+
+  hide() {
+    this.scrollToTopButtonElement.classList.remove(this.stateClasses.isVisible)
+  }
+
+  onScrollToTopButtonElement() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    })
+  }
+
+  bindEvents() {
+    this.scrollToTopButtonElement.addEventListener('click', () => this.onScrollToTopButtonElement())
+    window.addEventListener('scroll', () => this.checkScrollPosition())
+  }
+}
+
+class ScrollButtonCollection {
+  constructor() {
+    this.init()
+  }
+
+  init() {
+    document.querySelectorAll(inputWrapperSelector).forEach((element) => {
+      new ScrollButton(element)
+    })
+  }
+}
+
+export default ScrollButtonCollection
